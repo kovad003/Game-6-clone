fee14a3bcfbb82a46d34aa0f522289982f65b55a
kovad003 <kovacsdansan@gmail.com>
2022-11-29 12:31:57 +0200

    Player can move to all directions, sprite can be flipped around the X-axis
     - Player is controlled through the rigidbody (rigidbody.velocity)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,23 +8,48 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     /* EXPOSED FIELDS */
-    
+    [SerializeField] private float moveSpeed = 5.0f;
     
     /* HIDDEN FIELDS */
     private Vector2 _rawInput;
-    
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D _rigidbody;
+
+    private void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
+        FlipSprite();
     }
     
+    /* FUNCTIONS */
+    private void Move()
+    {
+        // Vector3 delta = _rawInput * (moveSpeed * Time.deltaTime);
+        // transform.position += delta;
+        
+        // Vector2 playerVelocity = new Vector2 (_rawInput.x * moveSpeed, _rigidbody.velocity.y);
+        // _rigidbody.velocity = playerVelocity;
+        
+        Vector2 playerVelocity = new Vector2 (_rawInput.x * moveSpeed, _rawInput.y * moveSpeed);
+        _rigidbody.velocity = playerVelocity;
+
+        // bool playerHasHorizontalSpeed = Mathf.Abs(_rigidbody.velocity.x) > Mathf.Epsilon;
+
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(_rigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2 (Mathf.Sign(_rigidbody.velocity.x), 1f);
+        }
+    }
+
     /* EVENT FUNCTIONS */
     private void OnMove(InputValue inputValue)
     {
