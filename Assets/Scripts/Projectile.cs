d684a383eed2e517195ce56f3890884eb72e89b0
Nyyri <75020126+Nyyri@users.noreply.github.com>
2022-12-01 00:33:02 +0200

    Collision detection

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -10,15 +10,11 @@ using UnityEngine;
 public class Projectile : MonoBehaviour {
 
     /* EXPOSED FIELDS: */
-    [SerializeField] float _speed = 10f;
+    public float _speed = 10f;
 
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
-
-    // Update is called once per frame
-    void Update() {
-        
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(!collision.gameObject.CompareTag("Player")) {
+            Destroy(gameObject);
+        }
     }
 }
