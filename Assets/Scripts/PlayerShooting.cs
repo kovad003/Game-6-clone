17766a985a15db8cede0ab4a2f991a8d0f368bcd
Nyyri <75020126+Nyyri@users.noreply.github.com>
2022-12-01 00:32:36 +0200

    Shooting controls
    Attached to the player.
    Is using the new Input System to detect left click.

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -1,19 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// AUTHOR: @Nuutti J.
+/// Last modified: 1 Dec. 2022 by @Nuutti J.
+/// </summary>
 
 public class PlayerShooting : MonoBehaviour {
 
-    
+    /* EXPOSED FIELDS: */
+    [SerializeField] InputActionReference _shootInput;
 
+    /* HIDDEN FIELDS: */
+    Weapon _equippedWeapon;
+    bool isShooting;
 
-    // Start is called before the first frame update
+    // Subscribtion of the shoot button to PerformShoot method
+    void OnEnable() {
+        _shootInput.action.performed += PerformShoot;
+        _shootInput.action.canceled += PerformShoot;
+    }
+
+    // Unsubscribe
+    void OnDisable() {
+        _shootInput.action.performed -= PerformShoot;
+        _shootInput.action.canceled -= PerformShoot;
+    }
+
     void Start() {
-        
+        _equippedWeapon = GetComponentInChildren<Weapon>();
     }
 
     // Update is called once per frame
     void Update() {
-        
+        // If the shoot button is pressed or held down
+        if(isShooting) {
+            _equippedWeapon.Shoot();
+        }
+    }
+
+    /* FUNCTIONS */
+    void PerformShoot(InputAction.CallbackContext context) {
+        // Check that the player has a weapon child
+        if(_equippedWeapon == null) {
+            Debug.Log("Player equipped weapon is null ", gameObject);
+            return;
+        }
+
+        // Clicked / held
+        if (context.performed) {
+            isShooting = true;
+        }
+
+        // Released
+        if (context.canceled) {
+            isShooting = false;
+        }
     }
 }
