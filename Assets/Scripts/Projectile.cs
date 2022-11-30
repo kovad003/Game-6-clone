43a2cf481bfc74d43802b0c8b499b9c046bae8f0
Nyyri <75020126+Nyyri@users.noreply.github.com>
2022-11-30 14:27:45 +0200

    Creation

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
-{
+/// <summary>
+/// AUTHOR: @Nuutti J.
+/// Last modified: 30 Nov 2022 by @Nuutti J.
+/// </summary>
+
+public class Projectile : MonoBehaviour {
+
+    /* EXPOSED FIELDS: */
+    [SerializeField] float _speed = 10f;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 }
