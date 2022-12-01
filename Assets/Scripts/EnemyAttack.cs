4b9d117a8b1c8962b6c3ea957384bc53f6e5816e
kovad003 <kovacsdansan@gmail.com>
2022-12-01 16:14:03 +0200

    Improved the collision detection
     - Enemy will be kicked with an impulsive force
     - If you dodge enemy will be also pushed back

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 /// <summary>
 /// AUTHOR: @Joona H.
-/// Last modified: 1 Dec 2022 by @Joona H.
+/// Last modified: 1 Dec 2022 by @Daniel K.
 /// </summary>
 public class EnemyAttack : MonoBehaviour
 {
@@ -17,21 +17,28 @@ public class EnemyAttack : MonoBehaviour
     /* HIDDEN FIELDS: */
     private Rigidbody2D rigidbody;
     private PlayerHealth playerHealth;
-    
-    
+
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
         playerHealth = FindObjectOfType<PlayerHealth>();
     }
 
+    // @Daniel K - 01.12.2022
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("kick back");
-            rigidbody.velocity = kbForceModifier * -1 * rigidbody.velocity.normalized;
+            rigidbody.AddForce(kbForceModifier * -1 * rigidbody.velocity.normalized, ForceMode2D.Impulse);
             playerHealth.TakeDamage(damage);
         }
     }
+
+    // @Daniel K - 01.12.2022
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            rigidbody.AddForce(kbForceModifier * rigidbody.velocity.normalized, ForceMode2D.Impulse);
+    }
+    
 }
