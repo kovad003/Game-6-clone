a3af2786d0364ccc077af20c2323dcbce37396cc
joonajah <joona.heinonen@windowslive.com>
2022-12-01 11:53:05 +0200

    EnemyAttack
    Author for EnemyAttack

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// AUTHOR: @Joona H.
+/// Last modified: 1 Dec 2022 by @Joona H.
+/// </summary>
 public class EnemyAttack : MonoBehaviour
 {
     // Start is called before the first frame update
