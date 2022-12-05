ceb4bf435812d46493689ac083086dca5574cdd0
kovad003 <kovacsdansan@gmail.com>
2022-12-05 11:43:42 +0200

    Prefabbed game objects

--- Assets/Scripts/LevelExitDetector.cs
+++ Assets/Scripts/LevelExitDetector.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelDetector : MonoBehaviour
+public class LevelExitDetector : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D other)
     {
